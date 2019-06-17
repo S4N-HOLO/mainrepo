78ef8175cf45e9b2ef06e7c28cfff160c7c67067
S4N-HOLO <sc.fatman@gmail.com>
2019-06-17 23:18:21 +0300

    SDF

---
+++ day10/ex03/ft_any.c
@@ -0,0 +1,19 @@
+int	ft_any(char **tab, int(*f)(char*))
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (tab[i][j] != 0)
+		if (tab[i][j] != 0)
+		{	
+			f(tab[i][j]);
+			if (NULL == f(tab[i][j])
+					return (0);
+			else
+				return(1);
+			j++;
+		}
+		i++;
+}	
